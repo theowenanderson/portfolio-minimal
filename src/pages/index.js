875caacc67d650438b4c1fd64e9bc567fb20ebf2
Owen <owena@knights.ucf.edu>
2022-06-26 18:52:39 -0400

    Adding basic configurations to a stable build.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,23 @@
 import React from "react";
 import {
   AboutSection,
-  ArticlesSection,
-  ContactSection,
   HeroSection,
-  InterestsSection,
   Page,
-  ProjectsSection,
   Seo,
 } from "gatsby-theme-portfolio-minimal";
 
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="theowenanderson.dev" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="About Me" />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
         <InterestsSection sectionId="details" heading="Details" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <ContactSection sectionId="github" heading="Issues?" /> */}
       </Page>
     </>
   );
